f50d42339d058f7dd685b034ab04f0585ecb8832
AnirudhMoopan <anirudhmoopan@gmail.com>
2023-01-24 16:45:37 +0530

    calculator project finished

--- script.js
+++ script.js
@@ -10,3 +10,9 @@ function back() {
     var value = document.getElementById("screen").value;
     document.getElementById("screen").value = value.substr(0, value.length - 1);
 }
+function equalclick()
+{
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById("screen").value=result
+}
