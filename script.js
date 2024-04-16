03fc1aa936ed9f114c510d6bf971f886b7a29e36
AnirudhMoopan <anirudhmoopan@gmail.com>
2024-04-16 12:50:26 +0530

    modified and done:)

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-function numberclick(val)
-{
-    document.getElementById("screen").value+=val
+function numberclick(val) {
+  document.getElementById("screen2").value += val;
 }
-function clearscreen()
-{
-    document.getElementById("screen").value=""
+function clearscreen() {
+  document.getElementById("screen2").value = "";
 }
 function back() {
-    var value = document.getElementById("screen").value;
-    document.getElementById("screen").value = value.substr(0, value.length - 1);
+  var value = document.getElementById("screen2").value;
+  document.getElementById("screen2").value = value.substr(0, value.length - 1);
 }
-function equalclick()
-{
-    var text=document.getElementById("screen").value
-    var result=eval(text)
-    document.getElementById("screen").value=result
+function equalclick() {
+  var text = document.getElementById("screen2").value;
+  var result = eval(text);
+  document.getElementById("screen2").value = result;
 }
